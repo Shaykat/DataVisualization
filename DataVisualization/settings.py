4c45a0f7b16c58a1585486e0922c9f623c2fd84a
Md Abdul Mutalab Shaykat <shaykat2057@gmail.com>
2020-03-17 17:59:24 -0600

    [VISUAL] modify settings APPS portion and Root urls.py file.

--- DataVisualization/settings.py
+++ DataVisualization/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     'visual.apps.VisualConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
