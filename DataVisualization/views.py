f9883296e31daec72bfbb191ceaf5a370dfa660d
Md Abdul Mutalab Shaykat <shaykat2057@gmail.com>
2020-04-14 11:22:42 -0600

    [VISUAL] Fix issue of not getting table

--- DataVisualization/views.py
+++ DataVisualization/views.py
@@ -1,9 +1,27 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from visual.models import CovidObservation
+from visual.views import get_queryset
+
 
 def index(request):
-    context = {}
-    return render(request, './index.html', context)
+    template = loader.get_template('./index.html')
+    context_object_name = 'observation_list'
+    search_query = ""
+    latest_date = CovidObservation.objects.latest('observation_date').observation_date
+
+    if request.method == 'GET':
+        search_query = request.GET.get('country', "")
+    queryset = get_queryset(search_query, 'country')
+
+    context = {
+        context_object_name: queryset.filter(observation_date=latest_date)
+    }
+
+    if request.is_ajax():
+        return JsonResponse(data=context, safe=False)
+
+    return HttpResponse(template.render(context, request))
