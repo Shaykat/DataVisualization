278638e0c7a8dd28280594cd904ca9277f8737e0
Md Abdul Mutalab Shaykat <shaykat2057@gmail.com>
2021-05-10 04:19:23 -0600

    [DATA VISUALIZATION] update the settings file

--- DataVisualization/settings.py
+++ DataVisualization/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*&r(_pz(j$e!!aa7-o4kzg+&q!+r#i)jajkcod&7uzb3*q6!yk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
