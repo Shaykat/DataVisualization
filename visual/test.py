d4d87f8afa129af472b05bfe34d77a3cc86cc86e
Md Abdul Mutalab Shaykat <shaykat2057@gmail.com>
2021-05-11 00:06:31 -0600

    [DATA VISUALIZATION] update Test Script

--- visual/test.py
+++ visual/test.py
@@ -6,13 +6,13 @@ from django.utils import timezone
 from .models import CovidObservation
 
 
+def latest_date_check(start_date, end_date):
+    return start_date < end_date
+
+
 class CovidObservationModelTests(TestCase):
 
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+    def test_latest_observation_date(self):
+        latest_date = CovidObservation.objects.latest('observation_date').observation_date
+        date = datetime.today()
+        self.assertIs(latest_date_check(latest_date, date), False)
